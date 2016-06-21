a6304a2093fd1b0abcf350ebfa02fe6bc3cf6518
koyot9110 <jozeflukac91@gmail.com>
2016-06-21 16:00:45 +0200

    test2

---
+++ src/stone/Field.java
@@ -0,0 +1,215 @@
+package stone;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Field {
+
+	private BufferedReader input = new BufferedReader(new InputStreamReader(
+			System.in));
+	private final int[][] array;
+	private final int rowCount;
+	private final int columnCount;
+	private final int stoneCount;
+	private long startTime = System.currentTimeMillis();
+	private long time = 0;
+	Random rnd = new Random();
+
+	public Field(int rowCount, int columnCount, int stoneCount) {
+		this.rowCount = rowCount;
+		this.columnCount = columnCount;
+		this.stoneCount = stoneCount;
+		array = new int[rowCount][columnCount];
+	}
+
+	public Field() {
+		this(4, 4, 15);
+	}
+
+	public int getRowCount() {
+		return rowCount;
+	}
+
+	public int getColumnCount() {
+		return columnCount;
+	}
+
+	public int getStoneCount() {
+		return stoneCount;
+	}
+
+	public int getArray(int row, int column) {
+		return array[row][column];
+	}
+
+	public long getTime(long time) {
+		time = ((System.currentTimeMillis() - startTime)) / 1000;
+		return time;
+	}
+
+	public void setTime(long time) {
+		this.time = time;
+	}
+
+	public void newGameStarted() throws WrongFormatException {
+		generate();
+		sufle();
+		do {
+			printBoard();
+			System.out.println("Enter: 'new' for new game");
+			System.out.println("Enter: 'exit' for exit game");
+			System.out.println("Enter: 'w, a, s, d' for move");
+			System.out.println("Time is: " + getTime(time) + " second");
+			processInput();
+		} while (true);
+	}
+
+	private void generate() {
+		int count = 0;
+		for (int i = 0; i < getRowCount(); i++) {
+			for (int j = 0; j < getColumnCount(); j++) {
+				array[i][j] = count;
+				count++;
+			}
+
+		}
+	}
+
+	private void sufle() throws WrongFormatException {
+		int increment = 0;
+		do {
+			int random = rnd.nextInt(4);
+			switch (random) {
+			case 0:
+				handleInput("w");
+				break;
+			case 1:
+				handleInput("a");
+				break;
+			case 2:
+				handleInput("s");
+				break;
+			case 3:
+				handleInput("d");
+				break;
+			default:
+				break;
+			}
+			increment++;
+		} while (increment < 50);
+	}
+
+	private void printBoard() {
+		for (int i = 0; i < getRowCount(); i++) {
+			for (int j = 0; j < getColumnCount(); j++) {
+				if (array[i][j] < 10) {
+					System.out.print(array[i][j] + "  ");
+				} else {
+					System.out.print(array[i][j] + " ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+	private void handleInput(String input) throws WrongFormatException {
+		Pattern pattern = Pattern
+				.compile("(new)?(exit)?([w]{1}|[a]{1}|[s]{1}|[d]{1}|up{1}|left{1}|down{1}|right{1})?");
+		Matcher matcher = pattern.matcher(input);
+
+		if (matcher.matches()) {
+			if (matcher.group(1) != null && matcher.group(1).equals("new")) {
+				System.out.println("New game!");
+				newGameStarted();
+			} else if (matcher.group(2) != null
+					&& matcher.group(2).equals("exit")) {
+				System.out.println("Exit game!");
+				System.exit(0);
+			} else if (matcher.group(3) != null) {
+				move(matcher);
+			}
+		} else {
+			System.out.println("Wrong format input");
+			processInput();
+		}
+	}
+
+	private void move(Matcher matcher) {
+		for (int i = 0; i < getRowCount(); i++) {
+			for (int j = 0; j < getColumnCount(); j++) {
+				if (array[i][j] == 0) {
+					switch (matcher.group(3)) {
+					case "w":
+					case "up":
+						if (i < getRowCount() - 1) {
+							array[i][j] = getArray(i + 1, j);
+							array[i + 1][j] = 0;
+							System.out.println("Move up");
+						}
+						break;
+					case "a":
+					case "left":
+						if (j < getColumnCount() - 1) {
+							array[i][j] = getArray(i, j + 1);
+							array[i][j + 1] = 0;
+							System.out.println("Move left");
+						}
+						break;
+					case "s":
+					case "down":
+						if (i > 0) {
+							array[i][j] = getArray(i - 1, j);
+							array[i - 1][j] = 0;
+							System.out.println("Move down");
+						}
+						break;
+					case "d":
+					case "right":
+						if (j > 0) {
+							array[i][j] = getArray(i, j - 1);
+							array[i][j - 1] = 0;
+							System.out.println("Move right");
+						}
+						break;
+					}
+					return;
+				}
+			}
+		}
+	}
+
+	private boolean isSolved() {
+		boolean state = false;
+		for (int i = 0; i < getRowCount(); i++) {
+			for (int j = 0; j < getColumnCount(); j++) {
+				if (array[i][j] == array[i][j + 1] - 1
+						&& array[getRowCount()][getColumnCount()] == 0) {
+					state = true;
+				} else {
+					state = false;
+				}
+			}
+		}
+		return state;
+	}
+
+	private void processInput() {
+		try {
+			handleInput(readLine());
+		} catch (WrongFormatException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private String readLine() {
+		try {
+			return input.readLine();
+		} catch (IOException e) {
+			return null;
+		}
+	}
+}
---
+++ src/stone/Main.java
@@ -0,0 +1,9 @@
+package stone;
+
+public class Main {
+
+	public static void main(String[] args) throws WrongFormatException {
+		Field game = new Field();
+		game.newGameStarted();
+	}
+}
